1229ea714d5b802f92363c8b1cbaddcf70bd2f8c
miguelmonterodev <miguelmonterodev@gmail.com>
2025-08-25 07:43:51 +0100

    provide text via command line arguments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, count_words, sort_on, sorted_list
+import sys
 
 def get_book_text(filepath):
     with open(filepath, "r") as f:
@@ -6,16 +7,20 @@ def get_book_text(filepath):
     return file_content
 
 def main():
-    content = get_book_text("books/frankenstein.txt")
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(get_num_words(content))
-    print("--------- Character Count -------")
-    new_list = sorted_list(count_words(content))
-    for item in new_list:
-        print(f"{item["char"]}: {item["num"]}")
-    print("============= END ===============")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        content = get_book_text(sys.argv[1])
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count ----------")
+        print(get_num_words(content))
+        print("--------- Character Count -------")
+        new_list = sorted_list(count_words(content))
+        for item in new_list:
+            print(f"{item["char"]}: {item["num"]}")
+        print("============= END ===============")
 
 
 main()
